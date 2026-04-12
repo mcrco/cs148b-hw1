a8b5a4f6a54c71f7237f611291a5ce1c4eaee290
mcrco <marco.yang.ca@gmail.com>
2026-04-12 02:18:44 -0700

    small change in activation

--- eecs148b_hw1/modules/activation.py
+++ eecs148b_hw1/modules/activation.py
@@ -10,7 +10,7 @@ def softmax(x: torch.Tensor, dim: int) -> torch.Tensor:
     # log sum exp trick:
     # exp(v_i) / sum(exp(v_i)) = exp(v_i - max(v)) / sum(exp(v_i - max(v)))
     # because we just multiplied numerator and denominator by exp(-max(v)).
-    x = x - x.amax(dim=dim).unsqueeze(dim)
+    x = x - x.amax(dim=dim, keepdim=True)
     numers = torch.exp(x)
-    denoms = numers.sum(dim=dim).unsqueeze(dim)
+    denoms = numers.sum(dim=dim, keepdim=True)
     return numers / denoms
